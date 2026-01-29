a1486514f831ff140386a1efdadff61534dbcbd4
zwen-yirochi <jijohn.vega@gmail.com>
2026-01-29 10:13:28 +0900

    chore(next.config) : 이미지 허용 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
                 hostname: 'api.dicebear.com',
             },
         ],
